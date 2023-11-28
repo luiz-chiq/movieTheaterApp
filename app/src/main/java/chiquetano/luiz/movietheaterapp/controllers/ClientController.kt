9eea26b5f5ed5200b2803ffe74af671ca33935bd
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 20:30:52 -0300

    refactor(ClientController): change the use of Dao

--- app/src/main/java/chiquetano/luiz/movietheaterapp/controllers/ClientController.kt
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/controllers/ClientController.kt
@@ -8,18 +8,17 @@ import chiquetano.luiz.movietheaterapp.views.MainActivity
 
 class ClientController (private val mainActivity: MainActivity) {
 
-    var sessionDao = FakeFilmSessionDao()
 
     fun buyTicket(sessionId: Int, seat: String, user: User) {
         if(user.userType == UserType.CLIENT){
             var ticket = Ticket(-1, user, seat)
-            sessionDao.addTicketsToSession(sessionId, ticket)
+            FakeFilmSessionDao.addTicketsToSession(sessionId, ticket)
         }
     }
 
     fun getSessions(user: User) {
         if(user.userType == (UserType.CLIENT))
-            sessionDao.getAll()
+            FakeFilmSessionDao.getAll()
 
     }
 
