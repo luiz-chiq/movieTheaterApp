c8ab604429ca3899fe2985f1d7c08dc0e46cf666
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 05:54:26 -0300

    feat(controllers): create ClientController

---
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/controllers/ClientController.kt
@@ -0,0 +1,20 @@
+package chiquetano.luiz.movietheaterapp.controller
+
+import chiquetano.luiz.movietheaterapp.model.Ticket
+import chiquetano.luiz.movietheaterapp.model.User
+import chiquetano.luiz.movietheaterapp.model.UserType
+import chiquetano.luiz.movietheaterapp.repositories.FakeFilmSessionDao
+import chiquetano.luiz.movietheaterapp.views.MainActivity
+
+class ClientController (private val mainActivity: MainActivity) {
+
+    var sessionDao = FakeFilmSessionDao()
+
+    fun buyTicket(sessionId: Int, seat: String, user: User) {
+        if(user.userType == UserType.CLIENT){
+            var ticket = Ticket(-1, user, seat)
+            sessionDao.addTicketsToSession(sessionId, ticket)
+        }
+    }
+
+}
