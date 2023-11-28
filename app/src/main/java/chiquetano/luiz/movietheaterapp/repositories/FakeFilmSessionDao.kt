8149e9fc5a4819e5e36194691105a230cf73bfc0
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 20:28:16 -0300

    refactor: made FakeFilmSessionDao 'static'

--- app/src/main/java/chiquetano/luiz/movietheaterapp/repositories/FakeFilmSessionDao.kt
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/repositories/FakeFilmSessionDao.kt
@@ -3,44 +3,46 @@ package chiquetano.luiz.movietheaterapp.repositories
 import chiquetano.luiz.movietheaterapp.model.FilmSession
 import chiquetano.luiz.movietheaterapp.model.Ticket
 
-class FakeFilmSessionDao() : GenericDao<FilmSession, Int> {
+class FakeFilmSessionDao() {
+    companion object {
 
-    private val filmSessionMap = mutableMapOf<Int, FilmSession>()
-    private var currentId = 1
+        private val filmSessionMap = mutableMapOf<Int, FilmSession>()
+        private var currentId = 1
 
-    override fun create(filmSession: FilmSession): Int {
-        val newId = currentId++
-        filmSessionMap[newId] = filmSession.copy(id = newId)
-        return newId
-    }
-
-    fun addTicketsToSession(id: Int, ticket: Ticket){
-        filmSessionMap[id]?.seats?.add(ticket)
-    }
-
-    override fun getOne(id: Int): FilmSession? {
-        return filmSessionMap[id]
-    }
-
-    override fun getAll(): MutableList<FilmSession> {
-        return filmSessionMap.values.toMutableList()
-    }
-
-    override fun update(filmSession: FilmSession): Int {
-        val existingSession = filmSessionMap[filmSession.id]
-        if (existingSession != null) {
-            filmSessionMap[filmSession.id] = filmSession
-            return filmSession.id
+        fun create(filmSession: FilmSession): Int {
+            val newId = currentId++
+            filmSessionMap[newId] = filmSession.copy(id = newId)
+            return newId
         }
-        return -1
-    }
 
-    override fun delete(id: Int): Int {
-        if (filmSessionMap.containsKey(id)) {
-            filmSessionMap.remove(id)
-            return id
+        fun addTicketsToSession(id: Int, ticket: Ticket) {
+            filmSessionMap[id]?.seats?.add(ticket)
         }
-        return -1
+
+        fun getOne(id: Int): FilmSession? {
+            return filmSessionMap[id]
+        }
+
+        fun getAll(): MutableList<FilmSession> {
+            return filmSessionMap.values.toMutableList()
+        }
+
+        fun update(filmSession: FilmSession): Int {
+            val existingSession = filmSessionMap[filmSession.id]
+            if (existingSession != null) {
+                filmSessionMap[filmSession.id] = filmSession
+                return filmSession.id
+            }
+            return -1
+        }
+
+        fun delete(id: Int): Int {
+            if (filmSessionMap.containsKey(id)) {
+                filmSessionMap.remove(id)
+                return id
+            }
+            return -1
+        }
     }
 
 
