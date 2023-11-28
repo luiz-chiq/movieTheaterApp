726a11c84b9380e4c8052aad6b4c0fe5d7cdc8e0
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 05:58:57 -0300

    feat(views): create ClientActivity

---
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/views/ClientActivity.kt
@@ -0,0 +1,16 @@
+package chiquetano.luiz.movietheaterapp.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import chiquetano.luiz.movietheaterapp.R
+
+class ClientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sessions)
+
+    }
+}
