87f476554a66c9385d22900e7b544e3221a62030
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 20:28:39 -0300

    refactor: made FakeTicketDao 'static'

--- app/src/main/java/chiquetano/luiz/movietheaterapp/repositories/FakeTicketDao.kt
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/repositories/FakeTicketDao.kt
@@ -2,36 +2,36 @@ package chiquetano.luiz.movietheaterapp.repositories
 
 import chiquetano.luiz.movietheaterapp.model.Ticket
 
-class FakeTicketDao() : GenericDao<Ticket, Int> {
+class FakeTicketDao(){
+    companion object {
+        private val ticketMap = mutableMapOf<Int, Ticket>()
+        private var currentId = 1
 
-    private val ticketMap = mutableMapOf<Int, Ticket>()
-    private var currentId = 1
-
-    override fun create(ticket: Ticket): Int {
-        val newId = currentId++
-        ticketMap[newId] = ticket.copy(id = newId)
-        return newId
-    }
-
-    override fun getOne(id: Int): Ticket? {
-        return ticketMap[id]
-    }
-
-    override fun getAll(): MutableList<Ticket> {
-        return ticketMap.values.toMutableList()
-    }
-
-    override fun update(ticket: Ticket): Int {
-        return -1
-    }
-
-    override fun delete(id: Int): Int {
-        if (ticketMap.containsKey(id)) {
-            ticketMap.remove(id)
-            return id
+        fun create(ticket: Ticket): Int {
+            val newId = currentId++
+            ticketMap[newId] = ticket.copy(id = newId)
+            return newId
         }
-        return -1
-    }
 
+        fun getOne(id: Int): Ticket? {
+            return ticketMap[id]
+        }
+
+        fun getAll(): MutableList<Ticket> {
+            return ticketMap.values.toMutableList()
+        }
+
+        fun update(ticket: Ticket): Int {
+            return -1
+        }
+
+        fun delete(id: Int): Int {
+            if (ticketMap.containsKey(id)) {
+                ticketMap.remove(id)
+                return id
+            }
+            return -1
+        }
+    }
 
 }
