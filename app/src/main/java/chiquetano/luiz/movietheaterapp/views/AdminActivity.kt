206d91200a098bffeb8d2bde19fb908fc3c02eea
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 05:55:09 -0300

    feat(views): create AdminActivity

---
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/views/AdminActivity.kt
@@ -0,0 +1,16 @@
+package chiquetano.luiz.movietheaterapp.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import chiquetano.luiz.movietheaterapp.R
+
+class AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_session)
+
+    }
+}
