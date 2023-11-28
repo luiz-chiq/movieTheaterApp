f2eed6c422e00e084506f43f5a9e4bea2bae4f08
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 20:31:06 -0300

    refactor(AuthController): change the use of Dao

--- app/src/main/java/chiquetano/luiz/movietheaterapp/controllers/AuthController.kt
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/controllers/AuthController.kt
@@ -7,20 +7,19 @@ import chiquetano.luiz.movietheaterapp.views.MainActivity
 
 class AuthController () {
 
-    var userDao = FakeUserDao()
     var authUser = ""
 
     fun signIn(username: String, password: String): Boolean {
-        if(userDao.getOne(username) != null) return false
+        if(FakeUserDao.getOne(username) != null) return false
         var user = User(username, password, UserType.CLIENT)
-        userDao.create(user)
+        FakeUserDao.create(user)
         authUser = username
         return true
     }
 
     fun logIn(username: String, password: String): Boolean {
-        if(userDao.getOne(username) == null) return false
-        if(userDao.getOne(username)!!.password != password) return false
+        if(FakeUserDao.getOne(username) == null) return false
+        if(FakeUserDao.getOne(username)!!.password != password) return false
         authUser = username
         return true
     }
@@ -30,7 +29,7 @@ class AuthController () {
     }
 
     fun userIsAdmin(): Boolean {
-        if (userDao.getUserType(authUser) == UserType.ADMIN) return true
+        if (FakeUserDao.getUserType(authUser) == UserType.ADMIN) return true
         return false
     }
 
