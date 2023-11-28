5fb5ec1382699609d34dd393678ca3dbab21b7a1
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 05:56:49 -0300

    feat(views): login button redirects to AdminActivity

--- app/src/main/java/chiquetano/luiz/movietheaterapp/views/MainActivity.kt
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/views/MainActivity.kt
@@ -1,12 +1,38 @@
 package chiquetano.luiz.movietheaterapp.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import chiquetano.luiz.movietheaterapp.R
+import chiquetano.luiz.movietheaterapp.controller.AuthController
 
 class MainActivity : AppCompatActivity() {
+
+    private val authController = AuthController()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBt: Button = findViewById(R.id.loginBt)
+        val signupBt: Button = findViewById(R.id.signup)
+        val usernameEt: EditText = findViewById(R.id.username)
+        val passwordEt: EditText = findViewById(R.id.password)
+
+        loginBt.setOnClickListener(View.OnClickListener {
+            var valid = authController.logIn(usernameEt.text.toString(), passwordEt.text.toString())
+            if(valid) {
+                val intent = Intent(this, AdminActivity::class.java)
+                startActivity(intent)
+            }
+            showToast("Username ou senha inválidos")
+        })
+
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
