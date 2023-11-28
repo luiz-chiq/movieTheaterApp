d23c2312d86e8ac173a0b86b4705381b4319bcf8
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-28 20:28:52 -0300

    refactor: made FakeUserDao 'static'

--- app/src/main/java/chiquetano/luiz/movietheaterapp/repositories/FakeUserDao.kt
+++ app/src/main/java/chiquetano/luiz/movietheaterapp/repositories/FakeUserDao.kt
@@ -3,44 +3,47 @@ package chiquetano.luiz.movietheaterapp.repositories
 import chiquetano.luiz.movietheaterapp.model.User
 import chiquetano.luiz.movietheaterapp.model.UserType
 
-class FakeUserDao() : GenericDao<User, String> {
-    private val userMap = mutableMapOf<String, User>()
+class FakeUserDao() {
 
-    init{
-        userMap["admin"] = User("admin", "123", UserType.ADMIN)
-    }
+    companion object {
+        private val userMap = mutableMapOf<String, User>()
 
-    override fun create(user: User): String {
-        userMap[user.username] = user
-        return user.username
-    }
+        init {
+            userMap["admin"] = User("admin", "123", UserType.ADMIN)
+        }
 
-    override fun getOne(username: String): User? {
-        return userMap[username]
-    }
-
-    override fun getAll(): MutableList<User> {
-        return userMap.values.toMutableList()
-    }
-
-    override fun update(user: User): String {
-        val existingUser = userMap[user.username]
-        if (existingUser != null) {
+        fun create(user: User): String {
             userMap[user.username] = user
             return user.username
         }
-        return ""
-    }
 
-    override fun delete(username: String): String {
-        if (userMap.containsKey(username)) {
-            userMap.remove(username)
-            return username
+        fun getOne(username: String): User? {
+            return userMap[username]
         }
-        return ""
-    }
 
-    fun getUserType(username: String): UserType {
-        return userMap[username]!!.userType
+        fun getAll(): MutableList<User> {
+            return userMap.values.toMutableList()
+        }
+
+        fun update(user: User): String {
+            val existingUser = userMap[user.username]
+            if (existingUser != null) {
+                userMap[user.username] = user
+                return user.username
+            }
+            return ""
+        }
+
+        fun delete(username: String): String {
+            if (userMap.containsKey(username)) {
+                userMap.remove(username)
+                return username
+            }
+            return ""
+        }
+
+        fun getUserType(username: String): UserType {
+            return userMap[username]!!.userType
+        }
     }
 }
